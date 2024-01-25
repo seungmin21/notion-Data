28fc7967d5fba5c902c9a1a804260d6387236f9c
ohseungmin <ohmin2886@gmail.com>
2024-01-25 11:07:13 +0900

    노션 페이지에서 rrr 제목의 출력을 확인

--- data.py
+++ data.py
@@ -1,23 +1,19 @@
 import requests
 
-# 노션 페이지의 ID와 통합 토큰 설정
-page_id = 'd15140f3-942b-4a63-bed4-a0c3cd54556f'
-notion_api_token = 'Bearer secret_VbYBREnh0zg7G60GIfpeQgBC8sCkoGb0zWqAOhy925u'
+notion_token = 'secret_Ar4jiLvCbf1pJzNfb4KYs6hK5HfXFFnUqT26TSIw2dY'
+notion_id = "8c9d9a905a074adf8397951fd399a459"
 
-# 노션 API 엔드포인트와 헤더 설정
-url = f'https://api.notion.com/v1/pages/{page_id}'
 headers = {
-    'Authorization': notion_api_token,
+    'Authorization' : f'Bearer {notion_token}',
     'Content-Type': 'application/json',
-    'Notion-Version': '2022-06-28',  # 유효한 Notion API 버전 사용
+    'Notion-Version': '2022-06-28',
 }
 
-# API 요청 보내기
-response = requests.get(url, headers=headers)
+response = requests.get(f'https://api.notion.com/v1/pages/{notion_id}', headers=headers)
 
-# 응답 확인
 if response.status_code == 200:
-    data = response.json()
-    print(data)
+    page_data = response.json()
+    print(page_data)
 else:
-    print(f"Error: {response.status_code} - {response.text}")
+    print(f"Failed to fetch Notion page. Status code: {response.status_code}")
+    print(response.text)
