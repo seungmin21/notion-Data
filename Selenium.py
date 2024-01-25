1337a74b132dc075b2719dfcd3600b9c3c24a738
ohseungmin <ohmin2886@gmail.com>
2024-01-25 10:27:20 +0900

    노션 페이지 로딩 완료까지 구현

--- Selenium.py
+++ Selenium.py
@@ -1,13 +1,16 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # Chrome 드라이버 초기화 (executable_path 전달하지 않음)
 driver = webdriver.Chrome()
 
 # 구글 홈페이지 열기
-driver.get('https://www.google.com')
+notion_url = 'https://www.notion.so/1-24-9bc07fb161084184b54fc6dc14e1f32a'
+driver.get(notion_url)
 
-# 브라우저 닫기
+# 웹 페이지가 완전히 로딩될 때까지 대기
+WebDriverWait(driver, 10).until(EC.title_contains('Expected Title'))
+
 driver.quit()
-
-# 라이브러리 업데이트로 인해 타입 에러가 발생하는 작성법
-# driver = webdriver.Chrome(executable_path=driver_path)
