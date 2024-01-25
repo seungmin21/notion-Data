bdb0942063df4ecfb04ffc5de5c2a00e5661c405
ohseungmin <ohmin2886@gmail.com>
2024-01-25 16:18:57 +0900

    결과물 파일

---
+++ Data.js
@@ -0,0 +1,17 @@
+const root = {
+  object: 'page',
+  id: '8c9d9a90-5a07-4adf-8397-951fd399a459',
+  created_time: '2024-01-24T09:40:00.000Z',
+  last_edited_time: '2024-01-25T02:45:00.000Z',
+  created_by: { object: 'user', id: '83bed4ba-72bc-43f3-8c6c-c838a4105f09' },
+  last_edited_by: { object: 'user', id: '83bed4ba-72bc-43f3-8c6c-c838a4105f09' },
+  cover: null,
+  parent: { type: 'workspace', workspace: true },
+  archived: false,
+  properties: { title: { id: 'title', type: 'title', title: [Array] } },
+  url: 'https://www.notion.so/rrr-8c9d9a905a074adf8397951fd399a459',
+  public_url: null,
+  request_id: 'ba1c9250-892a-46aa-b9d8-73450e61184c'
+}
+
+console.log(root.properties.title.title);
