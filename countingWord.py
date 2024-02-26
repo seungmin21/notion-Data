9a9c2bbe547de4296d3620d547b7d5dcad953e18
ohseungmin <ohmin2886@gmail.com>
2024-02-26 15:57:04 +0900

    특정 단어 한개 -> 특정 단어 두개로 변형 for in을 사용

--- countingWord.py
+++ countingWord.py
@@ -1,5 +1,5 @@
 # 파일 경로
-file_path = "./listData.py"
+file_path = "./testData.py"
 
 # 파일 열기 (텍스트 모드로 읽기)
 with open(file_path, "r", encoding="utf-8") as file:
@@ -7,8 +7,16 @@ with open(file_path, "r", encoding="utf-8") as file:
     data = file.read()
 
 # 읽은 데이터에서 특정 단어를 카운트
-target_word = "id"
-word_count = data.lower().count(target_word.lower())
+target_words = ["Response", "request"]
+
+word_counts = {}
+for target_word in target_words:
+    word_counts[target_word] = data.lower().count(target_word.lower())
+
+#word_count = data.lower().count(target_word.lower())
 
 # 결과 출력
-print(f"파일에서 단어 '{target_word}'는 총 {word_count}번 출현합니다.")
+for target_word, count in word_counts.items():
+    print(f"파일에서 단어 '{target_word}'는 총 {count}번 출현합니다.")
+
+#print(f"파일에서 단어 '{target_word}'는 총 {word_count}번 출현합니다.")
